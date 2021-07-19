5472f844af7ef5e1f497ac2c6c5af02fd7431e63
Andric34 <ivanand34@gmail.com>
2021-07-19 15:51:41 +0200

    Importing rating app client.

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    settings: {},
+    emoticons: []
   },
   mutations: {
+    setSettings (state, data) {
+      state.settings = data
+    },
+    setEmoticons (state, data) {
+      state.emoticons = data
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    getSettings (state) {
+      return state.settings
+    },
+    getEmoticons (state) {
+      return state.emoticons
+    }
   }
 })
---
+++ src/views/Public.vue
@@ -0,0 +1,33 @@
+<template>
+  <Rating v-bind:settings="settings"
+          v-bind:emoticons="emoticons"/>
+</template>
+
+<script>
+import Rating from '../../rating-app-client/src/views/rating'
+export default {
+  name: 'Public',
+  components: {
+    Rating
+  },
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    settings () {
+      return this.$store.state.settings
+    },
+    emoticons () {
+      return this.$store.state.emoticons
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
