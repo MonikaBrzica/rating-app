c7814c41076493bb56cdb2171bad458e63017ce5
Andric34 <ivanand34@gmail.com>
2021-07-21 14:05:01 +0200

    Connect settings inputs to database.

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,32 +1,32 @@
 <template>
-<div class="settings-container">
-  <div class="title-container">
-    <p class="title">Settings</p>
-    <p>X</p>
-  </div>
-  <div class="separator"></div>
-  <div class="main-container">
-    <div class="left-column">
+  <div class="settings-container">
+    <div class="title-container">
+      <p class="title">Settings</p>
+      <p>X</p>
+    </div>
+    <div class="separator"></div>
+    <div class="main-container">
       <div class="emoticons-preview-container">
         <p>Emotions preview</p>
-        <img v-for="item in this.emotionsArr"
-             v-bind:key="item.id"
-             :src="require('../assets/'+ item)"
-             alt="Emoticon"
-        >
+        <img
+          v-for="item in this.emotionsArr"
+          v-bind:key="item.id"
+          :src="require('../assets/' + item)"
+          alt="Emoticon"
+        />
       </div>
-      <Input v-bind:data="inputsArr[0]"/>
-    </div>
-    <div class="right-column">
-      <Input v-bind:data="inputsArr[1]"/>
-      <Input v-bind:data="inputsArr[2]"/>
+      <Input v-for="item in this.inputsArr"
+             v-bind:key="item.name"
+             v-bind:data="item"
+             @updateEmotions="changeSelectedEmotionsNum"
+             @updateSettings="updateSettings"/>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-import Input from './input'
+import Input from './inputComponent'
+import { HTTP } from '../../api/axios'
 export default {
   name: 'Settings',
   components: {
@@ -38,49 +38,67 @@ export default {
   },
   computed: {
     emotionsArr () {
-      return this.$store.getters.getEmotionsArr()
+      return this.$store.getters.getEmotionsArr(this.emotionsSelected)
     },
     inputsArr () {
       return [
         {
-          legend: 'Number of emotions',
-          text: 'Enter number from 3-5',
-          type: 'number',
-          placeholder: this.$store.state.settings.numOfEmoticons
-        },
-        {
+          name: 'msg',
           legend: 'Thank you message',
           text: '',
           type: 'text',
-          placeholder: this.$store.state.settings.msg
+          value: this.$store.getters.getSettings.msg
         },
         {
+          name: 'numOfEmoticons',
+          legend: 'Number of emotions',
+          text: 'Enter number from 3-5',
+          type: 'number',
+          value: this.$store.getters.getSettings.numOfEmoticons
+        },
+        {
+          name: 'timeout',
           legend: 'Message timeout',
           text: 'Can be from 0-15',
           type: 'number',
-          placeholder: this.$store.state.settings.timeout
+          value: this.$store.getters.getSettings.timeout
         }]
+    },
+    emotionsSelected () {
+      return this.settings.numOfEmoticons
+    },
+    settings () {
+      return this.$store.getters.getSettings
     }
   },
   methods: {
-  },
-  watch: {
-
+    changeSelectedEmotionsNum (event) {
+      this.settings.numOfEmoticons = event
+    },
+    updateSettings (data) {
+      const updatedSettings = this.settings
+      for (const property in updatedSettings) {
+        if (property === data.name) {
+          updatedSettings[property] = data.value
+        }
+      }
+      HTTP.put('/rating/settings', updatedSettings)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.settings-container{
+.settings-container {
   width: 1015px;
   height: 100vh;
+  box-sizing: border-box;
   background-color: #2D3038;
   padding: 2rem 3rem;
-  .title-container{
+  .title-container {
     padding: 34px 0 34px 16px;
     display: flex;
     justify-content: space-between;
-    align-items: center;
     p {
       color: rgba(255, 255, 255, 0.85);
       font-family: Roboto-Bold;
@@ -88,35 +106,31 @@ export default {
       line-height: 29px;
     }
   }
-
   .separator {
     background-color: rgba(255, 255, 255, 0.1);
-    box-shadow: 0 1px 0 0 rgba(128, 134, 139, 0.3), 0 -1px 0 0 rgba(128, 134, 139, 0.3);
+    box-shadow: 0 1px 0 0 rgba(128, 134, 139, 0.3),
+    0 -1px 0 0 rgba(128, 134, 139, 0.3);
     height: 1px;
     margin-bottom: 40px;
   }
-  .main-container{
+  .main-container {
     display: flex;
-    .left-column{
-      width: 48.5%;
-      margin-right: 1.5%;
-      .emoticons-preview-container{
-        color: rgba(255, 255, 255, 0.5);
-        img{
-          height: 24px;
-          width: 24px;
-          margin-right: 12px;
-        }
-        font-family: Roboto-Medium;
-        font-size: 12px;
-        line-height: 16px;
+    flex-direction: row;
+    flex-wrap: wrap;
+    column-gap: 24px;
+    row-gap: 20px;
+    .emoticons-preview-container {
+      color: rgba(255, 255, 255, 0.5);
+      flex-basis: calc(50% - 24px);
+      img {
+        height: 24px;
+        width: 24px;
+        margin-right: 12px;
       }
-    }
-    .right-column{
-      width: 48.5%;
-      margin-left: 1.5%;
+      font-family: Roboto-Medium;
+      font-size: 12px;
+      line-height: 16px;
     }
   }
 }
-
 </style>
---
+++ src/components/inputComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="input-container">
+    <fieldset>
+      <legend>{{ data.legend }}</legend>
+      <input v-bind:type="data.type"
+             v-bind:value=data.value
+             @change.stop="updateData (data.name, $event.target.value)">
+    </fieldset>
+    <p>{{ data.text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'inputComponent',
+  props: {
+    data: Object
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    updateData (name, value) {
+      if (name === 'numOfEmoticons') {
+        this.$emit('updateEmotions', parseInt(value))
+      }
+      if (name === 'msg') {
+        this.$emit('updateSettings', { name: name, value: value })
+      } else {
+        this.$emit('updateSettings', { name: name, value: parseInt(value) })
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.input-container{
+  flex-basis: calc(50% - 24px);
+  min-width: 280px;
+  fieldset{
+    border-radius: 5px;
+    border-color: rgba(255,255,255,0.2);
+    padding: 0;
+    legend{
+      color: rgba(255, 255, 255, 0.5);
+      font-family: Roboto-Medium;
+      font-size: 12px;
+      line-height: 16px;
+      padding: 0 8px;
+      margin-left: 8px;
+    }
+    input {
+      background-color: transparent;
+      outline: none;
+      border: none;
+      width: 100%;
+      height: 100%;
+      padding: 9px 16px;
+    }
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    input[type=number] {
+      -moz-appearance: textfield;
+    }
+  }
+  p{
+    color: rgba(255, 255, 255, 0.5);
+    font-family: Roboto-Regular;
+    font-size: 12px;
+    line-height: 16px;
+    margin: 4px;
+  }
+}
+
+</style>
