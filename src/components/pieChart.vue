fc894fabf3bad3633bb6cbe694e0d21c68d5889b
Monika Brzica <brzicamona@gmail.com>
2021-08-03 10:15:29 +0200

    refactor

--- src/components/pieChart.vue
+++ src/components/pieChart.vue
@@ -5,7 +5,7 @@
       <img src="../assets/images/dots-vertical.png"
       alt="...">
     </div>
-    <apexchart type="pie" width="380" :options="chartOptions" :series="series"></apexchart>
+    <apexchart type="pie" width="419" height="390" :options="chartOptions" :series="series"></apexchart>
   </div>
 </template>
 <script>
@@ -24,7 +24,6 @@ export default {
       series: [0, 0, 0, 0, 0],
       chartOptions: {
         chart: {
-          width: 380,
           type: 'pie',
           foreColor: 'rgba(255,255,255,0.85)',
           fill: {
@@ -36,17 +35,19 @@ export default {
           markers: {
             radius: 0
           },
-          padding: 5,
-          offsetX: 10
+          padding: 5
         },
         dataLabels: {
-          textAnchor: 'middle'
+          textAnchor: 'middle',
+          formatter: function (val) {
+            return Math.round(val)
+          }
         },
         labels: ['Very happy', 'Happy', 'Meh', 'Sad', 'Very sad'],
         colors: ['rgb(0, 168, 107)', 'rgb(65, 179, 233)', 'rgb(122, 122, 122)', 'rgb(255, 186, 19)', 'rgb(249, 88, 90)'],
         stroke: {
           colors: 'rgb(45, 48, 56)',
-          width: 2
+          width: 4
         },
         responsive: [{
           breakpoint: 480,
@@ -97,10 +98,12 @@ export default {
 <style scoped lang="scss">
 .pie-chart {
   background-color: $bg;
-  height: 100%;
-  width: 100%;
+  width: 419px;
+  height: 432px;
+  border-radius: 4px;
   .title-container {
-    padding: 20px;
+    padding: 10px 20px 20px 20px;
+    height: 24px;
     display: flex;
     justify-content: space-between;
     p {
--- src/components/reports.vue
+++ src/components/reports.vue
@@ -47,6 +47,13 @@ export default {
   .main-reports {
     width: calc(100% - 164px);
     order:2;
+    .pie-chart-container {
+      width: 100%;
+      height: 100%;
+      margin-top: 200px;
+      margin-right: 20px;
+      display: inline;
+    }
   }
 }
 @media only screen and (max-width: 768px) {
