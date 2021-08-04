e08fa13d4791e5f8e79b30e80b99bcbefabbb049
Monika Brzica <brzicamona@gmail.com>
2021-08-04 08:59:29 +0200

    add actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,20 @@
 export default {
   setRatings (context, data) {
     context.commit('setRatings', data)
+  },
+  setSettings (context, data) {
+    context.commit('setSettings', data)
+  },
+  setEmoticons (context, data) {
+    context.commit('setEmoticons', data)
+  },
+  loginUser (context, data) {
+    context.commit('loginUser', data)
+  },
+  logoutUser (context) {
+    context.commit('logoutUser')
+  },
+  setToken (context, data) {
+    context.commit('setToken', data)
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-<<<<<<< HEAD
 import state from './state'
 import mutations from './mutations'
 import getters from './getters'
 import actions from './actions'
-=======
-import state from './modules/state'
-import mutations from './modules/mutations'
-import getters from './modules/getters'
-import actions from './modules/actions'
 
->>>>>>> pie-chart
 Vue.use(Vuex)
 
 export default new Vuex.Store({
